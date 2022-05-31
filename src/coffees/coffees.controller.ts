9e8e8cb0bf2a9f2fcae9cdccac9d15656cc386ba
humbertoyusta <humbertoyusta02@gmail.com>
2022-05-31 17:23:54 -0400

    Deleted Coffee class

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,48 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
-class Coffee {
-    private _id : string;
-    private _type : string;
-
-	constructor(id: string, type: string) {
-		this._id = id;
-		this._type = type;
-	}
-
-    /**
-     * Getter id
-     * @return {string}
-     */
-	public get id(): string {
-		return this._id;
-	}
-
-    /**
-     * Getter type
-     * @return {string}
-     */
-	public get type(): string {
-		return this._type;
-	}
-
-    /**
-     * Setter id
-     * @param {string} value
-     */
-	public set id(value: string) {
-		this._id = value;
-	}
-
-    /**
-     * Setter type
-     * @param {string} value
-     */
-	public set type(value: string) {
-		this._type = value;
-	}
-    
-}
-
 @Controller('coffees')
 export class CoffeesController {
     @Get()
@@ -54,15 +11,15 @@ export class CoffeesController {
         return `Coffee ${id}`;
     }
     @Post()
-    create(@Body() body : Coffee) : string {
+    create(@Body() body) : string {
         return `creates a coffee, with id ${body.id}`;
     }
     @Delete(':id')
-    delete(@Param('id') id : string,@Body() body : Coffee) : string {
+    delete(@Param('id') id : string,@Body() body) : string {
         return `deletes the coffee with id ${id}`;
     }
     @Patch(':id')
-    update(@Param('id') id : string, @Body() body : Coffee) : string {
+    update(@Param('id') id : string, @Body() body) : string {
         return `updates the coffee with id ${id}`;
     }
 }
