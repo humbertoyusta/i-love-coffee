5865a9b775d6c8446e59b9506e8de69ab6622631
humbertoyusta <humbertoyusta02@gmail.com>
2022-05-31 22:27:55 -0400

    Added add and delete coffee to coffees.services

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -3,8 +3,24 @@ https://docs.nestjs.com/providers#services
 */
 
 import { Injectable } from '@nestjs/common';
+import { Coffee } from 'src/entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
+    private coffeesList : Coffee[] = [];
     
+    addCoffee (coffee : Coffee) : void {
+        this.coffeesList.push(coffee);
+    }
+
+    deleteCoffee (coffee : Coffee) : boolean {
+        let index : number = this.coffeesList.findIndex((c: Coffee) => (c.id == coffee.id));
+        if (index != -1)
+            this.coffeesList.splice(index, 1);
+        return index != -1;
+    }
+
+    getCoffees(): Coffee[] {
+        return this.coffeesList;
+    }
 }
