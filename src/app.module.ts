34e853cf90672620d533c0e4c59435d1ec8718e5
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-03 18:43:55 -0400

    added postgre with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
