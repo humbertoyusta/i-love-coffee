f6eaca39589025abb4b1e373e5a702fb1447368a
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-03 21:21:45 -0400

    removed unnecesary imports from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { CoffeesService } from './coffees/coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
