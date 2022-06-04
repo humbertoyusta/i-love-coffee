0dc63c79f7d613d5e37a37adf9336151a0fd946f
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-04 16:57:13 -0400

    Added many-to-many relation between coffees and flavours

--- src/coffees/entities/flavour.entity.ts
+++ src/coffees/entities/flavour.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Coffee } from "./coffee.entity";
 
 @Entity()
 export class Flavour {
@@ -7,4 +8,10 @@ export class Flavour {
 
     @Column()
     name: string;
+
+    @ManyToMany(
+        type => Coffee, 
+        (coffee) => coffee.flavours
+    )
+    coffees: Coffee[]
 }
