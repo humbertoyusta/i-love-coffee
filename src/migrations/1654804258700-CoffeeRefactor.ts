1ecd3677cf2c9c71c7720592465f69949cc313a1
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-09 15:54:58 -0400

    Created a migration (save of the current state of the db)

---
+++ src/migrations/1654804258700-CoffeeRefactor.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CoffeeRefactor1654804258700 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
