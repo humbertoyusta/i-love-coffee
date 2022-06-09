eecce897cc37395ee4a4a51d3a6619597fc2dc17
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-09 16:01:03 -0400

    Added sql queries to CoffeeRefactor Migration

--- src/migrations/1654804258700-CoffeeRefactor.ts
+++ src/migrations/1654804258700-CoffeeRefactor.ts
@@ -3,9 +3,11 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CoffeeRefactor1654804258700 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query('ALTER TABLE coffee RENAME COLUMN name TO title');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query('ALTER TABLE coffee RENAME COLUMN title TO name');
     }
 
 }
