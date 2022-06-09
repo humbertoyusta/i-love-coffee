289a1f6a3d343533f044f9962e008e240e5755ae
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-09 16:45:59 -0400

    tiny change in )

--- src/migrations/1654804258700-CoffeeRefactor.ts
+++ src/migrations/1654804258700-CoffeeRefactor.ts
@@ -3,11 +3,15 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CoffeeRefactor1654804258700 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query('ALTER TABLE coffee RENAME COLUMN name TO title');
+        await queryRunner.query(
+            'ALTER TABLE coffee RENAME COLUMN name TO title',
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query('ALTER TABLE coffee RENAME COLUMN title TO name');
+        await queryRunner.query(
+            'ALTER TABLE coffee RENAME COLUMN title TO name',
+        );
     }
 
 }
