cc344b349b148db6593f5d71879c85d34b8552fd
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-12 17:36:07 -0400

    Using async providers with useFactory

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,7 +2,7 @@
 https://docs.nestjs.com/modules
 */
 
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
@@ -11,12 +11,27 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavour } from './entities/flavour.entity';
 
+@Injectable()
+class CoffeeBrandsFactory {
+    async create(): Promise<string[]> {
+        const coffeeBrands = await Promise.resolve(['cubita', 'lallave', 'criollo']);
+        return coffeeBrands;
+    }
+}
+
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavour, Event])],
     controllers: [CoffeesController],
     providers: [
         CoffeesService, 
-        {provide: COFFEE_BRANDS, useValue: ['cubita', 'lallave', 'criollo']},
+        CoffeeBrandsFactory,
+        {
+            provide: COFFEE_BRANDS, 
+            useFactory: async (coffeeBrandsFactory: CoffeeBrandsFactory): Promise<string[]> => {
+                return coffeeBrandsFactory.create();
+            },
+            inject: [CoffeeBrandsFactory],
+        },
     ],
     exports: [CoffeesService],
 })
