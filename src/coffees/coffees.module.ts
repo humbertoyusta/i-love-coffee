e7030291289b26a60bc9ed76f30a9db92160c2a4
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-13 23:50:56 -0400

    testing registerAs and ConfigService.forFeature

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -8,11 +8,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import configCoffees from './coffees/config.coffees';
 import { Coffee } from './entities/coffee.entity';
 import { Flavour } from './entities/flavour.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Coffee, Flavour, Event]), ConfigModule],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavour, Event]), ConfigModule.forFeature(configCoffees)],
     controllers: [CoffeesController],
     providers: [CoffeesService],
     exports: [CoffeesService],
