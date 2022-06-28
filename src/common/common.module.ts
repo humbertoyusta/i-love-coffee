5b94425d0bfe70c4b539768a89ac8739ceac49ca
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-28 16:41:45 -0400

    Creating example middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ApiKeyGuard } from './guards/api-key.guard';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
     imports: [ConfigModule],
@@ -18,4 +19,8 @@ import { ApiKeyGuard } from './guards/api-key.guard';
         },
     ],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggingMiddleware).forRoutes('*');
+    }
+}
