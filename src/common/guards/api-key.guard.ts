b61ad9856cf3551d7074b5477165e316771380fc
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-27 22:36:12 -0400

    Adding public decorator to set methods that doesn't need auth header

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -2,12 +2,22 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 import appConfig from 'src/config/app.config';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler);
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('authorization');
 
