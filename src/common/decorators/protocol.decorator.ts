52e18a2babe20d2665bc689d7a23950b38b47e10
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-28 17:10:06 -0400

    using pass defaultValue to decorator

--- src/common/decorators/protocol.decorator.ts
+++ src/common/decorators/protocol.decorator.ts
@@ -5,7 +5,7 @@ https://docs.nestjs.com/openapi/decorators#decorators
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const Protocol = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (defaultValue: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     return request.protocol;
   },
