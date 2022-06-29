d91e3394bb23c27a18d600680b3fca7c855b8a16
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-28 23:35:07 -0400

    using @ApiProperty to show description and examples in api doc

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,8 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
 export class CreateCoffeeDto {
+    @ApiProperty({description: "The name of the coffee"})
     @IsString()
     name: string;
 
+    @ApiProperty({examples: ["cubita", "lallave"]})
     @IsString()
     brand: string;
 
