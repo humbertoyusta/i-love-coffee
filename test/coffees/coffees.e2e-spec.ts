e012cb5b7ad44440043b5459a02b17f034fddb07
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-30 11:24:58 -0400

    testing post endpoint

--- test/coffees/coffees.e2e-spec.ts
+++ test/coffees/coffees.e2e-spec.ts
@@ -3,29 +3,37 @@ import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 import { CoffeesService } from '../../src/coffees/coffees.service';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import appConfig from '../../src/config/app.config';
+import { CreateCoffeeDto } from '../../src/coffees/dto/create-coffee.dto';
+import { Coffee } from '../../src/coffees/entities/coffee.entity';
+import { Flavour } from '../../src/coffees/entities/flavour.entity';
+import { Event } from '../../src/events/entities/event.entity';
 
 describe('coffees', () => {
   let app: INestApplication;
+  const coffee = {
+    name: "a",
+    brand: "b",
+    flavours: ["c", "d"],
+  };
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [
         CoffeesModule,
-        TypeOrmModule.forRootAsync({
-            useFactory: () => ({
-                type: 'postgres',
-                host: 'localhost',
-                port: 5433,
-                username: 'postgres',
-                password: 'p123',
-                database: 'postgres',
-                autoLoadEntities: true,
-                synchronize: true,
-            })
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: 'postgres',
+          password: 'p123',
+          database: 'postgres',
+          entities: [Coffee, Flavour, Event],
+          autoLoadEntities: true,
+          synchronize: true,
         }),
       ],
       providers: [],
@@ -36,7 +44,19 @@ describe('coffees', () => {
     await app.init();
   }, 60000);
 
-  it.todo('Create [POST /]');
+  it('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then((request) => {
+        const expectedCoffee = {
+          ...coffee,
+          flavours: coffee.flavours.map(name => ({name})),
+        };
+        expect(request.body).toMatchObject(expectedCoffee);
+      });
+  }, 60000);
   it.todo('Get all [GET /]');
   it.todo('Get one [POST /:id]');
   it.todo('Delete one [DELETE /:id]');
