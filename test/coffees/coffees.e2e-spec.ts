87e5f177a7667e6a7253a9ce333fa9a54ef2f814
humbertoyusta <humbertoyusta02@gmail.com>
2022-06-29 22:06:08 -0400

    using test db for cofees e2e test

--- test/coffees/coffees.e2e-spec.ts
+++ test/coffees/coffees.e2e-spec.ts
@@ -1,24 +1,40 @@
 
 import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 import { CoffeesService } from '../../src/coffees/coffees.service';
 import { INestApplication } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import appConfig from '../../src/config/app.config';
 
 describe('coffees', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [CoffeesModule],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        CoffeesModule,
+        TypeOrmModule.forRootAsync({
+            useFactory: () => ({
+                type: 'postgres',
+                host: 'localhost',
+                port: 5433,
+                username: 'postgres',
+                password: 'p123',
+                database: 'postgres',
+                autoLoadEntities: true,
+                synchronize: true,
+            })
+        }),
+      ],
       providers: [],
     })
       .compile();
 
     app = moduleRef.createNestApplication();
     await app.init();
-  });
+  }, 60000);
 
   it.todo('Create [POST /]');
   it.todo('Get all [GET /]');
